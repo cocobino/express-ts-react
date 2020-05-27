50147ea913948fb23b2e095e24511c34be67e96a
이대경 <coolbino@naver.com>
2020-05-27 10:56:13 +0900

    config setting

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose');
-
-var uri = "mongodb://ldk0225:ldk0225@prccluster-shard-00-00-1kgh5.mongodb.net:27017,prccluster-shard-00-01-1kgh5.mongodb.net:27017,prccluster-shard-00-02-1kgh5.mongodb.net:27017/test?ssl=true&replicaSet=prcCluster-shard-0&authSource=admin&retryWrites=true&w=majority";
-mongoose.connect(uri);
+const dotenv = require('dotenv');
+dotenv.config();
+mongoose.connect(process.env.DB_URL);
